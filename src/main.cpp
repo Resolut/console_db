42abeae357ba4bc17a28548ede5852e491f27aa3
Dmitriy <samdimus@gmail.com>
2020-06-24 09:51:52 +0300

    добавлено определение функции showMenu

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,19 @@ int main()
 {
     return 0;
 }
+
+void showMenu(DbEngine& db)
+{
+    std::cout << "Количество клиентов в базе: " << db.getClientsCount() << std::endl;
+    std::cout << "МЕНЮ: " << std::endl;
+
+    std::cout << "0. Завершить программу" << std::endl;
+    std::cout << "1. Добавить Клиента" << std::endl;
+    std::cout << "2. Отобразить список всех клиентов" << std::endl;
+    std::cout << "3. Сохранить клиентов в файл" << std::endl;
+    std::cout << "4. Загрузить из файла" << std::endl;
+    std::cout << "5. Поиск клиента" << std::endl;
+    std::cout << "6. Удалить клиента из базы" << std::endl;
+
+    std::cout << ">>> ";
+}
