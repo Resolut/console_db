a9d4f5524f0ecd5ea634a51ef78d9e021c84ea7a
Dmitriy <samdimus@gmail.com>
2020-06-24 14:56:22 +0300

    Добавлены комментарии к заголовочному файлу класса DbEngine

--- src/dbengine.h
+++ src/dbengine.h
@@ -1,3 +1,12 @@
+/*!
+\file
+\brief Файл содержит класс DbEngine, представляющий сущность - базу данных.
+
+ Содержит методы для добавления, удаления, отображения и поиска данных,
+ а также методы чтения\записи базы в\из файл(а).  
+ 
+*/
+
 #ifndef DBENGINE_H
 #define DBENGINE_H
 
@@ -17,8 +26,8 @@ public:
     void savePeopleToFile();
 
 private:
-    int _clientCount;
-    std::vector<Person> people;
+    int _clientCount;           /// поле хранящее количество клиентов в базе
+    std::vector<Person> people; /// вектор для хранения клиентов
 };
 
 #endif // DBENGINE_H
