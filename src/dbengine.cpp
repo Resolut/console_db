9fca17356a0c4d15e802ed0a30577be293bf1a72
Dmitriy <samdimus@gmail.com>
2020-06-24 10:01:49 +0300

    добавлено определение функции добавление клиента addClient класса DbEngine

--- src/dbengine.cpp
+++ src/dbengine.cpp
@@ -1,5 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include "dbengine.h"
+#include "Person.h"
+
 DbEngine::DbEngine()
 {
     _clientCount = 0;
     people = {};
+}
+
+void DbEngine::addClient()
+{
+        std::string _name;
+        std::string _surname;
+        short _age;
+        std::string _phone;
+
+        std::cout << "Введите Имя: ";
+        std::cin >> _name;
+
+        std::cout << "Введите Фамилию: ";
+        std::cin >> _surname;
+
+        std::cout << "Введите возраст: ";
+        std::cin >> _age;
+
+        std::cout << "Введите номер телефона: ";
+        std::cin >> _phone;
+
+        people.push_back({_name, _surname, _age, _phone});
+        _clientCount++;
 }
