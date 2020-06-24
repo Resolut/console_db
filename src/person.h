b2633788c35b652f431b0e9ce820670e75778dce
Dmitriy <samdimus@gmail.com>
2020-06-24 09:54:40 +0300

    добавлен заголовочный файл структуры person.h

---
+++ src/person.h
@@ -0,0 +1,15 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <iostream>
+
+struct Person
+{
+  std::string name;
+  std::string surname;
+  short age;
+  std::string phone;
+};
+
+#endif // PERSON_H
+
